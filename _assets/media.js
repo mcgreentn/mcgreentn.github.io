b39ea24d0dc92a20144ec66e36bbe17d03191ce6
Michael Green <mcgreentn@gmail.com>
2021-10-24 20:02:20 -0400

    fall 2021 update
    - new publication page
    - new media page
    - updated carousel
    - TBD, new project cards

---
+++ _assets/media.js
@@ -0,0 +1,90 @@
+data = {
+    "publications": [
+        {
+            "year": 2020,
+            "author": "Will Douglas Heaven",
+            "publication": "Technology Review",
+            "title": "AI planners in Minecraft could help machines design better cities",
+            "link": "https://www.technologyreview.com/2020/09/22/1008675/ai-planners-minecraft-urban-design-healthier-happier-cities"
+        },
+        {
+            "year": 2020,
+            "author": "Matt Wille",
+            "publication": "Input Magazine",
+            "title": "'Minecraft' is being used to teach AI to how to do urban planning",
+            "link": "https://www.inputmag.com/design/minecraft-is-being-used-to-teach-ai-to-do-urban-planning"
+        },
+        {
+            "year": 2019,
+            "author": "Tommy Thompson",
+            "publication": "AI and Games",
+            "title": "Exploring the Generative Design in Minecraft Competition",
+            "link": "https://medium.com/@t2thompson/gdmc2019-b9474f18b959"
+        },
+        {
+            "year": 2018,
+            "author": "Dann Sullivan",
+            "publication": "Big Boss Battles",
+            "title": "Outwit time-travelling murderers in AI-generated Data Agent",
+            "link": "https://bigbossbattle.com/data-agent"
+        },
+        {
+            "year": 2018,
+            "author": "NYU Tandon",
+            "publication": "PR Newswire",
+            "title": "First-of-its-Kind Competition for Minecraft to Probe the Creative Potential of Artificial Intelligence",
+            "link": "https://www.prnewswire.com/news-releases/first-of-its-kind-competition-for-minecraft-to-probe-the-creative-potential-of-artificial-intelligence-300656527.html"
+        },
+        {
+            "year": 2016,
+            "author": "Lehigh University",
+            "publication": "Lehigh Summer Review",
+            "title": "Building Knowledge",
+            "link": "https://www1.lehigh.edu/news/building-knowledge"
+        },
+        {
+            "year": 2016,
+            "author": "Allison Labeau",
+            "publication": "The Brown and White",
+            "title": "Fourth mobiLEHIGH",
+            "link": "https://thebrownandwhite.com/2016/02/28/fourth-mobilehigh"
+        },
+        {
+            "year": 2016,
+            "author": "Emilie Moy",
+            "publication": "The Brown and White",
+            "title": "mobiLEHIGH",
+            "link": "https://thebrownandwhite.com/2015/03/30/mobilehigh"
+        },
+    ],
+    "media": [
+        {
+            "year": 2018,
+            "author": "NYU Tandon",
+            "publication": "The Future Of",
+            "title": "The Future Of Games",
+            "link": "https://podcasts.apple.com/us/podcast/the-future-of-games/id1436976053?i=1000423468308&mt=2"
+        },
+        {
+            "year": 2018,
+            "author": "Chrisoph Salge",
+            "publication": "PROCJAM",
+            "title": "Generative Design in Minecraft - Christoph Salge + Friends",
+            "link": "https://www.youtube.com/watch?v=7eVK8UAWOaw"
+        },
+        {
+            "year": 2017,
+            "author": "Michael C Green",
+            "publication": "EXAG at AIIDE",
+            "title": "Press Space to Fire",
+            "link": "https://www.youtube.com/watch?v=uI2wUiYuU8U&t=2336s"
+        },
+        {
+            "year": 2017,
+            "author": "Ahmed Khalifa and Michael C Green",
+            "publication": "COG",
+            "title": "General Video Game Rule Generation",
+            "link": "https://www.youtube.com/watch?v=fjMHNZGlzp4&list=PLg9kiaNi4TnUldHKNWJ_gSktaFfCE6ydc"
+        },
+    ]
+}
---
+++ _assets/research.js
@@ -0,0 +1,173 @@
+data = {
+    "papers": [
+        {
+            "title": "Physics-informed attention-based neural network for solving non-linear partial differential equations",
+            "authors": "Ruben Rodriguez-Torrado, Pablo Ruiz, Luis Cueto-Felgueroso, Michael Cerny Green, Tyler Friesen, Sebastien Matringe, Julian Togelius",
+            "conference": "arXiv",
+            "year": 2021,
+            "link": "https://arxiv.org/pdf/2105.07898.pdf"
+        },
+        {
+            "title": "Exploring open-ended gameplay features with Micro RollerCoaster Tycoon",
+            "authors": "Michael Cerny Green, Victoria Yen, Sam Earle, Dipika Rajesh, Maria Edwards, LB Soros",
+            "conference": "arXiv",
+            "year": 2021,
+            "link": "https://arxiv.org/pdf/2105.04342.pdf"
+        },
+        {
+            "title": "The AI Settlement Generation Challenge in Minecraft: First Year Report",
+            "authors": "Christoph Salge, Michael Cerny Green, Rodrigo Canaan, Filip Skwarski, Rafael Fritsch, Adrian Brightmoore, Shaofang Ye, Changxing Cao, Julian Togelius",
+            "conference": "arXiv",
+            "year": 2021,
+            "link": "https://arxiv.org/pdf/2103.14950.pdf"
+        },
+        {
+            "title": "Game Mechanic Alignment Theory and Discovery",
+            "authors": "Michael Cerny Green, Ahmed Khalifa, Philip Bontrager, Rodrigo Canaan, Julian Togelius",
+            "conference": "International Conference on the Foundations of Digital Games",
+            "year": 2021,
+            "link": "https://arxiv.org/abs/2102.10247.pdf"
+        },
+        {
+            "title": "Mech-Elites: Illuminating the Mechanic Space of GVG-AI",
+            "authors": "Megan Charity, Michael Cerny Green, Ahmed Khalifa, Julian Togelius",
+            "conference": "International Conference on the Foundations of Digital Games",
+            "year": 2021,
+            "link": "https://arxiv.org/pdf/2002.04733.pdf"
+        },
+        {
+            "title": "Mario level generation from mechanics using scene stitching",
+            "authors": "Michael Cerny Green, Luvneesh Mugrai, Ahmed Khalifa, Julian Togelius",
+            "conference": "IEEE Conference on Games",
+            "year": 2020,
+            "link": "https://arxiv.org/pdf/2002.02992.pdf"
+        },
+        {
+            "title": "Bootstrapping conditional gans for video game level generation",
+            "authors": "Ruben Rodriguez Torrado, Ahmed Khalifa, Michael Cerny Green, Niels Justesen, Sebastian Risi, Julian Togelius",
+            "conference": "IEEE Conference on Games",
+            "year": 2020,
+            "link": "https://arxiv.org/pdf/1910.01603.pdf"
+        },
+        {
+            "title": "The AI Settlement Generation Challenge in Minecraft",
+            "authors": "Christoph Salge, Michael Cerny Green, Rodrigo Canaan, Filip Skwarski, Rafael Fritsch, Adrian Brightmoore, Shaofang Ye, Changxing Cao, Julian Togelius",
+            "conference": "KI-Künstliche Intelligenz",
+            "year": 2020,
+            "link": "https://link.springer.com/article/10.1007/s13218-020-00635-0"
+        },
+        {
+            "title": "Automatic Critical Mechanic Discovery Using Playtraces in Video Games",
+            "authors": "Michael Cerny Green, Ahmed Khalifa, Gabriella Alves Bulhoes Barros, Tiago Machado, Julian Togelius",
+            "conference": "International Conference on the Foundations of Digital Games",
+            "year": 2019,
+            "link": "https://arxiv.org/pdf/1909.03094.pdf"
+        },
+        {
+            "title": "Organic building generation in minecraft",
+            "authors": "Michael Cerny Green, Christoph Salge, Julian Togelius",
+            "conference": "International Conference on the Foundations of Digital Games",
+            "year": 2019,
+            "link": "https://arxiv.org/pdf/1906.05094.pdf"
+        },
+        {
+            "title": "Two-step constructive approaches for dungeon generation",
+            "authors": "Michael Cerny Green, Ahmed Khalifa, Athoug Alsoughayer, Divyesh Surana, Antonios Liapis, Julian Togelius",
+            "conference": "International Conference on the Foundations of Digital Games",
+            "year": 2019,
+            "link": "https://arxiv.org/pdf/1906.04660.pdf"
+        },
+        {
+            "title": "Generative design in Minecraft: chronicle challenge",
+            "authors": "Christoph Salge, Christian Guckelsberger, Michael Cerny Green, Rodrigo Canaan, Julian Togelius",
+            "conference": "International Conference on Computational Creativity",
+            "year": 2019,
+            "link": "https://arxiv.org/pdf/1905.05888.pdf"
+        },
+        {
+            "title": "Tree search vs optimization approaches for map generation",
+            "authors": "Debosmita Bhaumik, Ahmed Khalifa, Michael Cerny Green, Julian Togelius",
+            "conference": "AAAI Conference on Artificial Intelligence and Interactive Digital Entertainment",
+            "year": 2019,
+            "link": "https://arxiv.org/pdf/1903.11678.pdf"
+        },
+        {
+            "title": "Evolutionarily-Curated Curriculum Learning for Deep Reinforcement Learning Agents",
+            "authors": "Michael Cerny Green, Benjamin Sergent, Pushyami Shandilya, Vibhor Kumar",
+            "conference": "Deep Reinforcement Learning in Games Workshop at AAAI 2019",
+            "year": 2019,
+            "link": "https://arxiv.org/pdf/1901.05431.pdf"
+        },
+        {
+            "title": "Intentional Computational Level Design",
+            "authors": "Ahmed Khalifa, Michael Cerny Green, Gabriella AB Barros, Julian Togelius",
+            "conference": "Genetic and Evolutionary Computation Conference",
+            "year": 2019,
+            "link": "https://dl.acm.org/doi/pdf/10.1145/3321707.3321849"
+        },
+        {
+            "title": "Data-driven Design: A Case for Maximalist Game Design",
+            "authors": "Gabriella AB Barros, Michael Cerny Green, Antonios Liapis, Julian Togelius",
+            "conference": "International Conference on Computational Creativity",
+            "year": 2018,
+            "link": "https://arxiv.org/pdf/1805.12475.pdf"
+        },
+        {
+            "title": "Generating Levels That Teach Mechanics",
+            "authors": "Michael Cerny Green, Ahmed Khalifa, Gabriella AB Barros, Andy Nealen, Julian Togelius",
+            "conference": "Procedural Content Generation Workshop at the International Conference on the Foundations of Digital Games",
+            "year": 2018,
+            "link": "https://arxiv.org/abs/1807.06734.pdf"
+        },
+        {
+            "title": "AtDelfi: Automatically Designing Legible, Full Instructions For Games",
+            "authors": "Michael Cerny Green, Ahmed Khalifa, Gabriella AB Barros, Tiago Machado, Andy Nealen, Julian Togelius",
+            "conference": "International Conference on the Foundations of Digital Games",
+            "year": 2018,
+            "link": "https://arxiv.org/abs/1807.04375.pdf"
+        },
+        {
+            "title": "DATA Agent",
+            "authors": "Michael Cerny Green, Gabriella AB Barros, Antonios Liapis, Julian Togelius",
+            "conference": "International Conference on the Foundations of Digital Games",
+            "year": 2018,
+            "link": "https://arxiv.org/abs/1810.02251.pdf"
+        },
+        {
+            "title": "Generative Design in Minecraft (GDMC), Settlement Generation Competition",
+            "authors": "Christoph Salge, Michael Cerny Green, Rodrigo Canaan, Julian Togelius",
+            "conference": "Procedural Content Generation Workshop at the International Conference on the Foundations of Digital Games",
+            "year": 2018,
+            "link": "https://arxiv.org/pdf/1803.09853.pdf"
+        },
+        {
+            "title": "Automated playtesting with Procedural Personas through MCTS with evolved heuristics",
+            "authors": "Christoffer Holmgard, Michael Cerny Green, Antonios Liapis, Julian Togelius",
+            "conference": "IEEE Transactions on Games",
+            "year": 2018,
+            "link": "https://arxiv.org/abs/1802.06881.pdf"
+        },
+        {
+            "title": "Who killed albert einstein? From open data to murder mystery games",
+            "authors": "Gabriella Alves Bulhoes Barros, Michael Green, Antonios Liapis, Julian Togelius",
+            "conference": "IEEE Transactions on Games",
+            "year": 2018,
+            "link": "https://arxiv.org/abs/1802.05219.pdf"
+        },
+        {
+            "title": "General Video Game Rule Generation",
+            "authors": "Ahmed Khalifa, Michael Cerny Green, Diego Perez-Liebana, Julian Togelius",
+            "conference": "IEEE Conference on Games",
+            "year": 2017,
+            "link": "https://arxiv.org/abs/1906.05160.pdf"
+        },
+        {
+            "title": "\"Press Space To Fire\": Automatic Video Game Tutorial Generation",
+            "authors": "Michael Cerny Green, Ahmed Khalifa, Gabriella AB Barros, Julian Togelius",
+            "conference": "Experimental AI in Games Workshop at the AAAI Conference on Artificial Intelligence and Interactive Digital Entertainment",
+            "year": 2017,
+            "link": "https://arxiv.org/abs/1805.11768.pdf"
+        },
+
+    ]
+};
