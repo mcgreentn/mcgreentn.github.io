c889e2c2a5ce6b3160bb86db4ea8ccb435d5d661
“Michael <michaelgreen@hitachisolutions.com>
2023-07-22 00:51:42 -0400

    actually add blog lol

---
+++ assets/blogs.js
@@ -0,0 +1,130 @@
+data = {
+    "posts": [
+        {
+            "title": "Practitioner's Insight",
+            "authors": "Michael C Green, Micah Groh",
+            "publication": "Medium - Hitachi Solutions",
+            "description": "We’ve decided to provide the critical eye with a side-by-side comparison scoped to training artificial intelligence models between Databricks (developers of Apache Spark’s latest evolution) and Snowflake.",
+            "date": "January 30, 2023",
+            "link": "https://medium.com/empoweranalytics/practitioners-insight-databricks-ai-suite-vs-snowflake-s-3rd-party-requirements-8d69f8f2a7a4",
+            "tags": ["professional"]
+        },
+        {
+            "title": "Practitioner's Experience",
+            "authors": "Michael C Green, Micah Groh",
+            "publication": "Medium - Hitachi Solutions",
+            "description": "We’ve decided to provide the critical eye with a side-by-side comparison scoped to training artificial intelligence models between Databricks (developers of Apache Spark’s latest evolution) and Snowflake.",
+            "date": "November 1, 2022",
+            "link": "https://medium.com/empoweranalytics/practitioners-experience-a-comparison-of-training-ml-models-using-snowpark-vs-databricks-f29d3f79fb6d",
+            "tags": ["professional"]
+        },
+        {
+            "title": "Practitioner's Perspective",
+            "authors": "Michael C Green, Mike Kirkpatrick",
+            "publication": "Medium - Hitachi Solutions",
+            "description": "In this article, we share where organizations will struggle with achieving value from Snowflake for advanced analytics, and why we cannot recommend Snowpark for AI/ML.",
+            "date": "October 17, 2022",
+            "link": "https://medium.com/empoweranalytics/practitioners-perspective-snowpark-f4fad4ab8995",
+            "tags": ["professional"]
+        },
+        {
+            "title": "Game Mechanic Alignment Theory",
+            "authors": "Michael C Green",
+            "publication": "WordPress",
+            "description": "This is post presents an easily (hopefully) digestible, non-technical overview of a paper with the same name, published at the International Conference on the Foundations of Digital Games in 2021. It was written in conjunction with Ahmed Khalifa, Philip Bontrager, Rodrigo Canaan, Julian Togelius, and myself.",
+            "date": "January 29, 2022",
+            "link": "https://mikecgreenblog.wordpress.com/2022/01/29/game-mechanic-alignment-theory/",
+            "tags": ["archive, research"]
+        },
+        {
+            "title": "Artificial Intelligence, Game Design, and Education: How it all fits",
+            "authors": "Michael C Green",
+            "publication": "WordPress",
+            "description": "Artificial intelligence has become a buzzword. Many people believe they know what AI is, but only a portion of them actually understand it. Just Google “AI”, and I guarantee there will be plenty of nonsense out there to read, like this article. But there is enormous potential in using AI to solve real-world problems for everyday people. In this post, I want to explain a really cool application for AI that I’ve dedicated the past few years of my life researching: AI as a teacher.",
+            "date": "July 18, 2018",
+            "link": "https://mikecgreenblog.wordpress.com/2018/07/18/artificial-intelligence-game-design-and-education-how-it-all-fits/",
+            "tags": ["archive, research"]
+        },
+        {
+            "title": "The Generative Design in Minecraft Competition (GDMC)",
+            "authors": "Michael C Green",
+            "publication": "WordPress",
+            "description": "The Generative Design in Minecraft Competition is designed to facilitate the exploration of new possibilities in the realm of 3-dimensional world generation and manipulation. The aim of this competition is to advance the state of the art of procedural generation of settlements and game content. The goal is to be able to produce Minecraft settlements of comparable quality to those produced by groups of human builders (see the section on Good Human Examples). In other words, developing an AI to build something that we consider functional AND pretty to look at.",
+            "date": "May 31, 2018",
+            "link": "https://mikecgreenblog.wordpress.com/2018/05/31/the-generative-design-in-minecraft-competition-gdmc/",
+            "tags": ["archive, research"]
+        },
+        {
+            "title": "The Legend of Zelda: Breath of the Wild Review",
+            "authors": "Michael C Green",
+            "publication": "WordPress",
+            "description": "I'm taking a Games for Learning class this semester, and one of my assignments was to choose a game I've never played before, play it for at least 20 hours, and then write up my game experience. I chose Breath of the Wild, as its a game I've been dying to play since it came out. I thought I'd post that write-up on the blog, both for those who have played Zelda and want a good discussion, and those who have yet to play it and have no idea what the hype is about.",
+            "date": "October 9, 2017",
+            "link": "https://mikecgreenblog.wordpress.com/2017/10/09/the-legend-of-zelda-breath-of-the-wild-review/",
+            "tags": ["archive"]
+        },
+        {
+            "title": "Professor A.I.",
+            "authors": "Michael C Green",
+            "publication": "WordPress",
+            "description": "We believe that AI has been used for several roles commonly in research such as content-creator, player, and student. Naturally there are more obscure roles that don't fit into these categories. We suggested a new role that has not been proposed yet: that of teacher. The problem is: If I have a game, can I generate a way to teach you, the player, how to play it?",
+            "date": "August 7, 2017",
+            "link": "https://mikecgreenblog.wordpress.com/2017/08/07/professor-a-i/",
+            "tags": ["archive, research"]
+        },
+        {
+            "title": "Education and Games",
+            "authors": "Michael C Green",
+            "publication": "WordPress",
+            "description": "My belief is that there are two ways to go about the infusion of video games and education. The first one of these is a very transparent meshing of the two. Gamify education. Make it easy to see that you took education and made a game out of it, like what we did with Edu-Venture. The second is to hide the education deep under layers, so the player has no idea that they were ever learning in the first place.",
+            "date": "March 4, 2017",
+            "link": "https://mikecgreenblog.wordpress.com/2017/03/04/education-and-games/",
+            "tags": ["archive, research"]
+        },
+        {
+            "title": "IS A.I. THE END?",
+            "authors": "Michael C Green",
+            "publication": "WordPress",
+            "description": "I want all of you to picture in your mind's eye, Arnold Schwarzenegger as the Terminator. Now, put that in a box somewhere in your mind and label that box NOT REAL. Artificial Intelligence is many decades, if not centuries, from ever approaching something remotely like a Terminator or SkyNet.",
+            "date": "January 23, 2017",
+            "link": "https://mikecgreenblog.wordpress.com/2017/01/23/is-a-i-the-end/",
+            "tags": ["archive, research"]
+        },
+        {
+            "title": "Open Data",
+            "authors": "Michael C Green",
+            "publication": "WordPress",
+            "description": "Open Data is information that is available to anyone that wants it. Wikipedia is a perfect example: it's free, it's easily available, and it's (mostly) true. Right now, the largest and most obvious use of open data is to look stuff up. That's a relatively straightforward use for it, but it makes you wonder, what else could you do with open data?",
+            "date": "November 14, 2016",
+            "link": "https://mikecgreenblog.wordpress.com/2016/11/27/open-data/",
+            "tags": ["archive, research"]
+        },
+        {
+            "title": "Intelligent Agent Personas",
+            "authors": "Michael C Green",
+            "publication": "WordPress",
+            "description": "I have the unique opportunity to study AI in the area of video games and game design. 'Video games?' you might exclaim. 'How can you call that a job?!'",
+            "date": "November 14, 2016",
+            "link": "https://mikecgreenblog.wordpress.com/2016/11/14/intelligent-agent-personas/",
+            "tags": ["archive, research"]
+        },
+        {
+            "title": "Virtual Reality is Cool.",
+            "authors": "Michael C Green",
+            "publication": "WordPress",
+            "description": "From my tiny NYU apartment above Union Square, you can hear the noisy city pulse with energy down below.  The smells of street vendors slither into the room; laughter, voices, shouting all echo off the steel walls surrounding my building. This is definitely a different way of life from the quiet suburbs of Germantown.",
+            "date": "June 21, 2016",
+            "link": "https://mikecgreenblog.wordpress.com/2016/06/21/virtual-reality-is-cool/",
+            "tags": ["archive"]
+        },
+        {
+            "title": "First Post",
+            "authors": "Michael C Green",
+            "publication": "WordPress",
+            "description": "A common thing among PhD students is to start a blog to record their thoughts. A journal of some sorts. A window into the inner stream of consciousness. There appears to be several schools of thought centered around the reasons behind it. Here's why I'm doing it now.",
+            "date": "June 11, 2016",
+            "link": "https://mikecgreenblog.wordpress.com/2016/06/11/first-blog-post/",
+            "tags": ["archive"]
+        },
+    ]
+};
