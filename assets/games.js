8edb4590e2db23f7a71b40307cd53a5bbda65ef1
Michael Green <mcg520@nyu.edu>
2026-03-26 15:09:17 -0400

    Add game data and previews for new titles in games.js; update play.html and projects.html for dynamic game display

---
+++ assets/games.js
@@ -0,0 +1,66 @@
+data = {
+    "games": [
+        {
+            "name": "AI Clicker",
+            "sub": "idle",
+            "url": "ai-clicker.html",
+            "bootTitle": "AI CLICKER",
+            "screenshot": "images/previews/aiclicker.png"
+        },
+        {
+            "name": "DunGen",
+            "sub": "roguelike",
+            "url": "dungen.html",
+            "bootTitle": "DUNGEN",
+            "screenshot": "images/previews/dungen.png"
+        },
+        {
+            "name": "Data Agent",
+            "sub": "mystery",
+            "url": "https://champchampchamp.itch.io/data-agent",
+            "bootTitle": "DATA AGENT",
+            "screenshot": "images/previews/data_agent.jpg",
+            "newTab": true
+        },
+        {
+            "name": "Seer",
+            "sub": "fiction",
+            "url": "https://champchampchamp.itch.io/seer",
+            "bootTitle": "SEER",
+            "screenshot": "images/previews/seer.png",
+            "newTab": true
+        },
+        {
+            "name": "Lifeguard",
+            "sub": "narrative",
+            "url": "https://champchampchamp.itch.io/lifeguard",
+            "bootTitle": "LIFEGUARD",
+            "screenshot": "images/previews/lifeguard.png",
+            "newTab": true
+        },
+        {
+            "name": "Trumpland",
+            "sub": "sim",
+            "url": "https://champchampchamp.itch.io/trumpland",
+            "bootTitle": "TRUMPLAND",
+            "screenshot": "images/previews/trumpland.png",
+            "newTab": true
+        },
+        {
+            "name": "2118: Corruption",
+            "sub": "rpg",
+            "url": "https://champchampchamp.itch.io/2118-corruption",
+            "bootTitle": "2118: CORRUPTION",
+            "screenshot": "images/previews/2118_corruption.png",
+            "newTab": true
+        },
+        {
+            "name": "Edu-Venture",
+            "sub": "edu",
+            "url": "https://champchampchamp.itch.io/edu-venture",
+            "bootTitle": "EDU-VENTURE",
+            "screenshot": "images/previews/eduventure.png",
+            "newTab": true
+        }
+    ]
+};
